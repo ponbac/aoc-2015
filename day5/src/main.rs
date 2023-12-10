9a80177aa0000e397b9dc2795e4556a008a475c5
ponbac <root@ponbac.xyz>
2023-12-10 18:49:14 +0100

    d5 prerp

---
+++ day5/src/main.rs
@@ -0,0 +1,19 @@
+static EXAMPLE_INPUT: &str = r#"abcdef"#;
+
+fn main() {
+    println!("\n-- Advent of Code 2015 - Day 5 --");
+
+    let input = EXAMPLE_INPUT;
+    // let input = include_str!("input.txt");
+
+    part1(input);
+    // part2(input);
+}
+
+fn part1(input: &str) {
+    todo!()
+}
+
+fn part2(input: &str) {
+    todo!()
+}
