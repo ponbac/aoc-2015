269b2d2dc51894b5b27b79ee8a87ac1fdce7aff5
ponbac <root@ponbac.xyz>
2023-12-14 14:34:18 +0100

    d8 shell

---
+++ day8/src/main.rs
@@ -0,0 +1,16 @@
+static EXAMPLE_INPUT: &str = r#"
+
+"#;
+
+fn main() {
+    println!("\n-- Advent of Code 2015 - Day 8 --");
+
+    // let input = EXAMPLE_INPUT;
+    let input = include_str!("input.txt");
+
+    solve(input);
+}
+
+fn solve(input: &str) {
+    todo!()
+}
