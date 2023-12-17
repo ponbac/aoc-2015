a0a4ee2e77f5e88ce8d785eb2fd9099b4d609e6d
ponbac <root@ponbac.xyz>
2023-12-17 19:44:36 +0100

    d11

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn main() {
     println!("\n-- Advent of Code 2015 - Day 11 --");
 
@@ -13,60 +15,87 @@ fn solve(input: &str) {
             break;
         }
     }
-
     println!("Part 1: {}", password);
+
+    loop {
+        password = next_password(&password);
+        if is_valid(&password) {
+            break;
+        }
+    }
+    println!("Part 2: {}", password);
 }
 
 fn next_password(password: &str) -> String {
-    let mut chars = password.chars().rev();
-    let mut next = String::new();
-
-    let mut carry = true;
-    while let Some(c) = chars.next() {
-        let c = if carry {
-            carry = false;
-            match c {
-                'z' => 'a',
-                _ => (c as u8 + 1) as char,
-            }
+    let mut chars: Vec<char> = password.chars().collect();
+    let mut i = chars.len() - 1;
+    loop {
+        let mut c = chars[i] as u8;
+        if c == b'z' {
+            c = b'a';
         } else {
-            c
-        };
+            c += 1;
+        }
+        chars[i] = c as char;
 
-        if c == 'i' || c == 'o' || c == 'l' {
-            next.push((c as u8 + 1) as char);
-            next.push_str(&"a".repeat(chars.count()));
+        if c != b'a' {
             break;
         }
 
-        if c == 'z' {
-            carry = true;
+        if i == 0 {
+            chars.insert(0, 'a');
+            break;
         }
 
-        next.push(c);
+        i -= 1;
     }
 
-    todo!()
+    chars.into_iter().collect()
 }
 
 fn is_valid(password: &str) -> bool {
-    todo!()
+    if password.contains('i') || password.contains('o') || password.contains('l') {
+        return false;
+    }
+
+    let mut increasing_straight = false;
+    let mut pairs = HashSet::new();
+
+    let chars: Vec<char> = password.chars().collect();
+    for i in 0..password.len() - 2 {
+        let a = chars[i] as u8;
+        let b = chars[i + 1] as u8;
+        let c = chars[i + 2] as u8;
+
+        if a + 1 == b && b + 1 == c {
+            increasing_straight = true;
+        }
+
+        if a == b {
+            pairs.insert((a, b));
+        } else if b == c {
+            pairs.insert((b, c));
+        }
+    }
+
+    increasing_straight && pairs.len() >= 2
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     use rstest::rstest;
+    use rstest::rstest;
 
-//     #[rstest(
-//         case("1", "11"),
-//         case("11", "21"),
-//         case("21", "1211"),
-//         case("1211", "111221"),
-//         case("111221", "312211")
-//     )]
-//     fn test_look_and_say(#[case] number: &str, #[case] expected: bool) {
-//         assert_eq!(result, expected);
-//     }
-// }
+    #[rstest(
+        case("hijklmmn", false),
+        case("abbceffg", false),
+        case("abbcegjk", false),
+        case("abcdffaa", true),
+        case("ghjaabcc", true)
+    )]
+    fn test_look_and_say(#[case] password: &str, #[case] expected: bool) {
+        let result = is_valid(password);
+        assert_eq!(result, expected);
+    }
+}
