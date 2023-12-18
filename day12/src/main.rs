81fbb948a3a20274418b4a9e0893233a607f3603
ponbac <root@ponbac.xyz>
2023-12-18 17:10:03 +0100

    d12

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,3 +1,5 @@
+use serde_json::Value;
+
 fn main() {
     println!("\n-- Advent of Code 2015 - Day 12 --");
 
@@ -6,36 +8,24 @@ fn main() {
 }
 
 fn solve(input: &str) {
-    let sum_1 = sum_numbers(input);
-    println!("Part 1: {}", sum_1);
+    let json = serde_json::from_str::<Value>(input).unwrap();
+    println!("Part 1: {}", sum_numbers(&json, false));
+    println!("Part 2: {}", sum_numbers(&json, true));
 }
 
-fn sum_numbers(input: &str) -> i64 {
-    let mut sum = 0;
-    let mut number_parts = String::new();
-    let mut is_negative = false;
-
-    for c in input.chars() {
-        match c {
-            '-' => is_negative = true,
-            '0'..='9' => number_parts.push(c),
-            _ => {
-                if !number_parts.is_empty() {
-                    let number = number_parts.parse::<i64>().unwrap();
-                    if is_negative {
-                        sum -= number;
-                    } else {
-                        sum += number;
-                    }
-
-                    number_parts = String::new();
-                    is_negative = false;
-                }
+fn sum_numbers(json: &Value, ignore_red: bool) -> i64 {
+    match json {
+        Value::Number(num) => num.as_i64().unwrap_or(0),
+        Value::Array(arr) => arr.iter().map(|v| sum_numbers(v, ignore_red)).sum(),
+        Value::Object(obj) => {
+            if ignore_red && obj.values().any(|v| v == "red") {
+                0
+            } else {
+                obj.values().map(|v| sum_numbers(v, ignore_red)).sum()
             }
         }
+        _ => 0,
     }
-
-    sum
 }
 
 #[cfg(test)]
@@ -54,7 +44,20 @@ mod tests {
         case(r#"[]"#, 0)
     )]
     fn test_p1(#[case] json: &str, #[case] expected: i64) {
-        let result = sum_numbers(json);
+        let json = serde_json::from_str::<Value>(json).unwrap();
+        let result = sum_numbers(&json, false);
+        assert_eq!(result, expected);
+    }
+
+    #[rstest(
+        case(r#"[1,2,3]"#, 6),
+        case(r#"[1,{"c":"red","b":2},3]"#, 4),
+        case(r#"{"d":"red","e":[1,2,3,4],"f":5}"#, 0),
+        case(r#"[1,"red",5]"#, 6)
+    )]
+    fn test_p2(#[case] json: &str, #[case] expected: i64) {
+        let json = serde_json::from_str::<Value>(json).unwrap();
+        let result = sum_numbers(&json, true);
         assert_eq!(result, expected);
     }
 }
